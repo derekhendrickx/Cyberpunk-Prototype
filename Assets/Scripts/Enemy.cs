c5ef3df9b35ece8c79d28df8b3f4d8c88b1c04ad
Derek Hendrickx <derek@hendrickx.me>
2017-01-23 22:52:24 -0500

    Create an enemy

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class Enemy : MonoBehaviour {
+
+	public float damage = 0f;
+	public float health = 100f;
+	public float speed = 1f;
+	private ThirdPersonCharacter thirdPersonCharacter;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		thirdPersonCharacter = gameObject.GetComponentInChildren<ThirdPersonCharacter>();
+		thirdPersonCharacter.MoveSpeedMultiplier = speed;
+		thirdPersonCharacter.AnimSpeedMultiplier = speed;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/// <summary>
+	/// OnTriggerEnter is called when the Collider other enters the trigger.
+	/// </summary>
+	/// <param name="other">The other Collider involved in this collision.</param>
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.CompareTag("Player"))
+		{
+			// TODO: Apply damage
+		}
+	}
+}
