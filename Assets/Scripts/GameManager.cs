b4339e133340626db9c9d1f12ba8d57d722a6ab1
Derek Hendrickx <derek@hendrickx.me>
2017-01-23 22:20:06 -0500

    Add the game manager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null; 
+
+	private GameObject player;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);   
+		}
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
+		
+		Init();
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void Init()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+	}
+}
